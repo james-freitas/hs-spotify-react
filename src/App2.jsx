c56cfcd3e0d7c22dd19a86e4d37d3464d7067caa
James Freitas <jamesfrj@yahoo.com.br>
2025-02-15 11:55:03 -0300

    Project initial structure

---
+++ src/App2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componente
+// PascalCase
