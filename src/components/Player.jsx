eb5eafd50ce4c1be501c6ff22473d4a9f269f659
James Freitas <jamesfrj@yahoo.com.br>
2025-02-16 02:50:01 -0300

    Add song component and Player

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCirclePlay,
+  faBackwardStep,
+  faForwardStep,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="player__controllers">
+        <Link to="/song/1">
+          <FontAwesomeIcon className="player__icon" icon={faBackwardStep} />
+        </Link>
+
+        <Link to="/song/2">
+          <FontAwesomeIcon
+            className="player__icon player__icon--play"
+            icon={faCirclePlay}
+          />
+        </Link>
+
+        <Link to="/song/3">
+          <FontAwesomeIcon className="player__icon" icon={faForwardStep} />
+        </Link>
+      </div>
+
+      <div className="player__progress">
+        <p>00:00</p>
+        <div className="player__bar">
+          <div className="player__bar-progress"></div>
+        </div>
+        <p>02:31</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,5 +1,37 @@
+import Player from "../components/Player";
+import { Link } from "react-router-dom";
+
 const Song = () => {
-  return <div>Song</div>;
+  return (
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+            alt="Song X"
+          />
+        </div>
+      </div>
+
+      <div className="song__bar">
+        <Link to="/artist/1" className="song__artist-image">
+          <img
+            width={75}
+            height={75}
+            src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+            alt="Artist X"
+          />
+        </Link>
+
+        <Player />
+
+        <div>
+          <p className="song__name">Última Saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Song;
